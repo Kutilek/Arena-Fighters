f60f6d6c99b09297bf63a1632527410d8d91e6d8
Kutilek <flamekick97@gmail.com>
2020-08-18 10:51:35 +0200

    Created Input Handler class

---
+++ Arena-Fighters-Unity-Project/Assets/Player/Scripts/Input_Handler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public abstract class Input_Handler : MonoBehaviour
+{
+    protected Player_Controller commandHandler;
+    protected bool helperKeyPressed;
+    protected KeyCode[] inputKeys;
+    protected KeyCode helperKey;
+    
+    protected virtual void Start()
+    {
+        commandHandler = GetComponent<Player_Controller>();
+    }
+
+    protected void GetHelperKeyPressed()
+    {
+        if (Input.GetKey(helperKey))
+            helperKeyPressed = true;
+        else
+            helperKeyPressed = false;
+    }
+
+    protected virtual Command CreateCommand(KeyCode[] inputKeys)
+    {
+        return new Command();
+    }
+}
