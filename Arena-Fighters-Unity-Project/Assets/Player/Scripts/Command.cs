bff69f05f59ef561bb982f940832a17ad0d11d9a
Kutilek <flamekick97@gmail.com>
2020-08-18 10:42:36 +0200

    Changed Command Struct
    Added variables to Command Struct according to what will be needed for input

--- Arena-Fighters-Unity-Project/Assets/Player/Scripts/Command.cs
+++ Arena-Fighters-Unity-Project/Assets/Player/Scripts/Command.cs
@@ -6,10 +6,14 @@ public struct Command
 {
     public KeyCode keyCode;
     public bool released;
+    public bool doublePressed;
+    public bool helperKeyPressed;
     
-    public Command(KeyCode keyCode, bool released)
+    public Command(KeyCode keyCode, bool released, bool doublePressed, bool helperKeyPressed)
     {
         this.keyCode = keyCode;
         this.released = released;
+        this.doublePressed = doublePressed;
+        this.helperKeyPressed = helperKeyPressed;
     }
 }
