1f29f29d716accd2dcf5d6d5e822a0c099d95b10
Kutilek <flamekick97@gmail.com>
2020-08-18 11:08:31 +0200

    Created Movement Input Handler

---
+++ Arena-Fighters-Unity-Project/Assets/Player/Scripts/Movement_Input_Handler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Movement_Input_Handler : Input_Handler
+{
+    private KeyCode lastPressed;
+    private float doublePressTimer;
+
+    public float doublePressTime;
+    
+    protected override void Start()
+    {
+        base.Start();
+        helperKey = KeyCode.LeftShift;
+        inputKeys = new KeyCode[5] {KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.Space};
+    }
+
+    void Update()
+    {
+        GetHelperKeyPressed();
+    }
+
+    protected override Command CreateCommand(KeyCode[] inputKeys)
+    {
+        foreach(KeyCode inputKey in inputKeys)
+        {
+            if(Input.GetKeyDown(inputKey) && lastPressed == inputKey) 
+            {
+                lastPressed = KeyCode.None;
+                if(Time.time - doublePressTimer < doublePressTime) 
+                {
+                    doublePressTimer = 0f;
+                    return new Command(inputKey, false, true, helperKeyPressed);
+                }
+            }
+
+            if (Input.GetKey(inputKey))
+                return new Command(inputKey, false, false, helperKeyPressed);
+
+            if (Input.GetKeyUp(inputKey))
+            {
+                lastPressed = inputKey;
+                doublePressTimer = Time.time;
+                return new Command(inputKey, true, false, helperKeyPressed);
+            }
+        }
+        return new Command();
+    }
+}
