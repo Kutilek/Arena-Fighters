e97fa85b87c073609be09c38935683109df62e6c
Kutilek <flamekick97@gmail.com>
2020-09-14 14:50:02 +0200

    Cleaned Code a Little

--- Arena-Fighters-Unity-Project/Assets/Player/Scripts/Player_Controller.cs
+++ Arena-Fighters-Unity-Project/Assets/Player/Scripts/Player_Controller.cs
@@ -20,25 +20,26 @@ public class Player_Controller : MonoBehaviour
 
     #endregion
 
+    // Physics & Movement
+    private readonly float gravity = Physics.gravity.y;
+    public Transform groundCheck;
+    private LayerMask groundMask;
+    private float velocityY;
+    private float groundDistance = 0.1f;
+    private bool isGrounded;
+    private Vector3 velocity;
+    private Vector3 currentImpact;
+    [SerializeField] protected float damping;
+    [SerializeField] protected float mass;
+    private float currentSpeed;
+    private float dashBonus;
+    private bool wallRunning;
+    private bool falling;
+
     private CharacterController controller;
     private Transform cam;
     private Transform arenaCenter;
-    public Transform groundCheck;
-    private LayerMask groundMask;
-    private float turnSmoothVelocity;
-    private float turnSmoothTime = 0.05f;
-
-    private readonly float gravity = Physics.gravity.y;
-    private float groundDistance = 0.1f;
-    private bool isGrounded;
-    private float currentSpeed;
-    private float dashBonus;
-    public bool wallRun;
-    public Vector3 velocity;
     
-    private Vector3 currentImpact;
-    public float velocityY;
-
     [SerializeField] protected float frontSpeed;
     [SerializeField] protected float sideSpeed;
     [SerializeField] protected float backSpeed;
@@ -46,8 +47,9 @@ public class Player_Controller : MonoBehaviour
     [SerializeField] protected float sideDashForce;
     [SerializeField] protected float backDashForce;
     [SerializeField] protected float jumpForce;
-    [SerializeField] protected float damping;
-    [SerializeField] protected float mass;
+    [SerializeField] protected float wallRunDuration;
+    [SerializeField] protected float dashDuration;
+
     
     void Start()
     {
@@ -61,74 +63,70 @@ public class Player_Controller : MonoBehaviour
     {
         Vector3 moveDir = new Vector3();
 
-        if (!wallRun)
-        {
-            HandleGravity();
+        SetSpeed();
+        CheckIfGrounded();
 
+        if (!wallRunning)
+        {
             if (doublePressMovementCommand.Equals(dashForward))
-            {
-                StartCoroutine(Dash(frontDashForce * dashBonus, 0f));
-            }
+                StartCoroutine(Dash(frontDashForce * dashBonus));
             else if (doublePressMovementCommand.Equals(dashBackward))
-            {
-                StartCoroutine(Dash(backDashForce * dashBonus, 180f));
-            }
+                StartCoroutine(Dash(backDashForce * dashBonus));
             else if (doublePressMovementCommand.Equals(dashLeft))
-            {
-                StartCoroutine(Dash(sideDashForce * dashBonus, 270f));
-            }   
+                StartCoroutine(Dash(sideDashForce * dashBonus));
             else if (doublePressMovementCommand.Equals(dashRight))
-            {
-                StartCoroutine(Dash(sideDashForce * dashBonus, 90f));
-            }
+                StartCoroutine(Dash(sideDashForce * dashBonus));
 
             moveDir = cam.transform.TransformDirection(direction);
+            RotatePlayer();
         }
         else
         {
+            transform.LookAt(arenaCenter);
+            transform.rotation = Quaternion.Euler(0f, transform.eulerAngles.y, 0f);
+            
             if (Mathf.Abs(direction.x) > 0.5f)
             {
                 float targetAngle = Mathf.Atan2(direction.x, 0f) * Mathf.Rad2Deg;
                 moveDir = Quaternion.Euler(0f, transform.eulerAngles.y - targetAngle, 0f) * Vector3.forward * 5f;
-            }
+            }   
 
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (pressMovementCommand.Equals(jump))
             {
                 AddForce(Quaternion.Euler(0f, transform.eulerAngles.y , 0f) * Vector3.forward, 100f);
                 StopCoroutine(WallRun());
-                wallRun = false;
+                wallRunning = false;
             }
-
-        }
+        }      
 
         velocity = moveDir * currentSpeed + Vector3.up * velocityY;
-        
-        CheckIfGrounded();
-        SetSpeed();
 
-        RotatePlayer(); 
+        if (!falling)
+        {    
+            if (pressMovementCommand.Equals(jump))
+                Jump();         
+        }
+        else
+            AddGravity(); 
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            Jump();
-
-        HandleCurrentImpact();
-
-        controller.Move(velocity * Time.deltaTime);  
+        CalculateCurrentImpact();
+        controller.Move(velocity * Time.deltaTime);
     }
 
     private IEnumerator WallRun()
     {
-        wallRun = true;
+        wallRunning = true;
+        falling = false;
         velocityY = 0f;
 
-        yield return new WaitForSeconds(80f);
+        yield return new WaitForSeconds(wallRunDuration);
 
         velocityY -= 5f;
-        wallRun = false;
-
+        wallRunning = false;
+        falling = true;
     }
 
-    public virtual void HandleCurrentImpact()
+    private void CalculateCurrentImpact()
     {
         if (currentImpact.magnitude > 0.2f)
         {
@@ -138,33 +136,33 @@ public class Player_Controller : MonoBehaviour
         currentImpact = Vector3.Lerp(currentImpact, Vector3.zero, damping * Time.deltaTime);
     } 
 
-    public void Jump()
+    private void Jump()
     {
-        if (isGrounded)
-        {
-            AddForce(Vector3.up, jumpForce);
-        }
+        falling = true;
+        AddForce(Vector3.up, jumpForce);
     }
 
-    void OnControllerColliderHit(ControllerColliderHit hit)
+    private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (hit.transform.tag == "Wall" && !isGrounded && velocity.y > -5f)
         {
             StartCoroutine(WallRun());
         }
-   }
+    }
 
-    public IEnumerator Dash(float dashForce, float plusDir)
+    private IEnumerator Dash(float dashForce)
     {
-        Vector3 dashDirection = Quaternion.Euler(0f, transform.eulerAngles.y + plusDir, 0f) * Vector3.forward;
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+
+        Vector3 dashDirection = Quaternion.Euler(0f, transform.eulerAngles.y + targetAngle, 0f) * Vector3.forward;
         AddForce(dashDirection, dashForce);
 
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(dashDuration);
 
         ResetImpact();
     }
 
-    public virtual void HandleGravity()
+    private void AddGravity()
     {
         if (isGrounded && velocityY < 0f)
         {
@@ -174,27 +172,27 @@ public class Player_Controller : MonoBehaviour
         velocityY += gravity * 1.5f * Time.deltaTime;
     }
 
-    public void AddForce(Vector3 dir, float magnitude)
+    private void AddForce(Vector3 dir, float magnitude)
     {
         currentImpact += dir.normalized * magnitude / mass;
     }
 
-    public void ResetImpact()
+    private void ResetImpact()
     {
         currentImpact = Vector3.zero;
     }
 
-    public void ResetImpactY()
-    {
-        currentImpact.y = 0f;
-    }
-
-    void CheckIfGrounded()
+    private void CheckIfGrounded()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded)
+        {
+            velocityY = 0f;
+            falling = false;
+        }      
     }
 
-    void SetSpeed()
+    private void SetSpeed()
     {
         dashBonus = 1f;
 
@@ -207,33 +205,38 @@ public class Player_Controller : MonoBehaviour
 
         if (moveHelperKeyPressed)
         {
-            currentSpeed *= 1.678423f;
-            dashBonus = 2f;
+            currentSpeed = CalculateSpeedBonus(currentSpeed);
+            dashBonus = CalculateDashBonus(dashBonus);
         }
     }
 
-    void RotatePlayer()
+    private float CalculateSpeedBonus(float speed)
     {
+        speed *= 1.678423f;
+        return speed;
+    }
 
-        
-        if (!wallRun)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float smoothRotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+    private float CalculateDashBonus(float bonus)
+    {
+        bonus = 2f;
+        return bonus;
+    }
 
-            if (direction.magnitude >= 0.1f)
-            {
-                if (direction.z > 0.5f)
-                    transform.rotation = Quaternion.Euler(0f, smoothRotation, 0f);
-                else
-                    transform.rotation = Quaternion.Euler(0f, cam.eulerAngles.y, 0f);  
-            } 
-        }
-        else
+
+    private float turnSmoothVelocity;
+    private float turnSmoothTime = 0.05f;
+
+    private void RotatePlayer()
+    {
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float smoothRotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+        if (direction.magnitude >= 0.1f)
         {
-            transform.LookAt(arenaCenter);
-            transform.rotation = Quaternion.Euler(0f, transform.eulerAngles.y, 0f);
-        }
-              
+            if (direction.z > 0.5f)
+                transform.rotation = Quaternion.Euler(0f, smoothRotation, 0f);
+            else
+                transform.rotation = Quaternion.Euler(0f, cam.eulerAngles.y, 0f);  
+        }    
     }
 }
