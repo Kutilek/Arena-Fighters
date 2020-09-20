ccc8d1070a684e991e51543882ff405237c4132a
Kutilek <flamekick97@gmail.com>
2020-09-20 11:09:46 +0200

    Created Enemy Controller Class

---
+++ Arena-Fighters-Unity-Project/Assets/Characters/Enemy/Scripts/Enemy_Controller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Enemy_Controller : Physics_Character_Controller
+{
+    private Transform player;
+    private Vector3 direction;
+
+    protected override void Start()
+    {
+        base.Start();    
+    }
+
+    protected void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    protected void Update()
+    {
+        CheckIfGrounded();
+
+        currentSpeed = 5f;
+
+        MoveCharacter(direction);
+        
+        RotateOnGround();
+    }
+
+    private void Jump()
+    {
+        AddForce(Vector3.up, 50f);
+        falling = true;
+    }
+
+    private float turnSmoothVelocity;
+    private float turnSmoothTime = 0.05f;
+    private void RotateOnGround()
+    {
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        float smoothRotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+        if (direction.magnitude >= 0.1f && !falling)
+        {
+            transform.rotation = Quaternion.Euler(0f, smoothRotation, 0f);
+        }    
+    }
+}
