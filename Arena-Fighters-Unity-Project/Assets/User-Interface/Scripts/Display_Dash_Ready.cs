44578c3a43434ef32f74d223ea30bc40e379648a
Kutilek <flamekick97@gmail.com>
2020-10-18 22:39:02 +0200

    Created User Interface

---
+++ Arena-Fighters-Unity-Project/Assets/User-Interface/Scripts/Display_Dash_Ready.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Display_Dash_Ready : MonoBehaviour
+{
+    private Text dashText;
+    private Dash dash;
+    private void Awake()
+    {
+        dashText = GetComponent<Text>();
+        dash = GameObject.FindGameObjectWithTag("Player").GetComponent<Dash>();
+    }
+
+    void Update()
+    {
+        if (!dash.GetDashed())
+        {
+            dashText.color = Color.green;
+            dashText.text = "Dash Is Ready";
+        }      
+        else
+        {
+            dashText.color = Color.red;
+            dashText.text = "Dash Is Not Ready";
+        }      
+    }
+}
---
+++ Arena-Fighters-Unity-Project/Assets/User-Interface/Scripts/Display_Health.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Display_Health : MonoBehaviour
+{
+    private Text healthText;
+    private Health health;
+    private void Awake()
+    {
+        healthText = GetComponent<Text>();
+        health = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+    }
+
+    void Update()
+    {
+        healthText.text = health.GetAmount().ToString();
+    }
+}
