2dfb57f987b26ced2f9cf960053789289916b8c7
Kutilek <flamekick97@gmail.com>
2020-10-25 22:11:07 +0100

    Player Rotate towards enemy on Attack

--- Arena-Fighters-Unity-Project/Assets/Characters/Player/Scripts/Player_Combat.cs
+++ Arena-Fighters-Unity-Project/Assets/Characters/Player/Scripts/Player_Combat.cs
@@ -1,4 +1,23 @@
-﻿public class Player_Combat : Character_Combat
-{
+﻿using UnityEngine;
 
+public class Player_Combat : Character_Combat
+{
+    public override Character_Physics GetCombatCharacterComponents(float distance, out Health health)
+    {
+        Collider[] collider = Physics.OverlapSphere(transform.position, distance, LayerMask.GetMask("Enemy"));
+        foreach (Collider col in collider)
+        {
+            if (col.GetComponent<Character_Combat>() != null)
+            {
+                Debug.Log(col);
+                transform.LookAt(col.transform);
+                Health targetHealth;
+                Character_Physics target = base.GetCombatCharacterComponents(distance, out targetHealth);
+                health = targetHealth;
+                return target;
+            }
+        }
+        health = null;
+        return null;
+    }
 }
