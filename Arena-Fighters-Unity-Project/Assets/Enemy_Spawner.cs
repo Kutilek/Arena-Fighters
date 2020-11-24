2297998055147061bc0d20600098b285fae664c6
Kutilek <flamekick97@gmail.com>
2020-11-24 21:38:52 +0100

    Enemy Spawner Prototype

---
+++ Arena-Fighters-Unity-Project/Assets/Enemy_Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Enemy_Spawner : MonoBehaviour
+{
+    public bool enemzSpawned;
+    public GameObject prefab;
+    public int numberOfEnemies;
+
+    void Update()
+    { 
+        if (!enemzSpawned)
+        {
+            for (int i = 0; i < numberOfEnemies; i++)
+                Instantiate(prefab, Vector3.zero, Quaternion.identity);
+
+            enemzSpawned = true;
+            StartCoroutine(SpawnEnemy());
+        }
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        yield return new WaitForSeconds(2f);
+
+        enemzSpawned = false;
+    }
+}
