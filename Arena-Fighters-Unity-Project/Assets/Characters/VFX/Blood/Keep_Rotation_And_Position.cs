2e53977226fc55d884b60c63836f5e99a33afd51
Ondřej Pavelka <flamekick97@gmail.com>
2021-02-25 13:40:21 +0100

    Created Basic Hit and Blood VFX

---
+++ Arena-Fighters-Unity-Project/Assets/Characters/VFX/Blood/Keep_Rotation_And_Position.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Keep_Rotation_And_Position : MonoBehaviour
+{
+    private Quaternion startRotation;
+    private Vector3 startPosition;
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+    }
+    
+    private void LateUpdate()
+    {
+        transform.rotation = startRotation;
+        transform.position = startPosition;
+    }
+}
