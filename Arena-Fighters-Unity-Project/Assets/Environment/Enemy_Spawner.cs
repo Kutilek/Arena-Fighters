51b842600ab796c6bb10c284520b84ecce98344d
Ondřej Pavelka <flamekick97@gmail.com>
2021-03-12 09:27:12 +0100

    Enemy Spawning

--- Arena-Fighters-Unity-Project/Assets/Environment/Enemy_Spawner.cs
+++ Arena-Fighters-Unity-Project/Assets/Environment/Enemy_Spawner.cs
@@ -3,26 +3,50 @@ using UnityEngine;
 
 public class Enemy_Spawner : MonoBehaviour
 {
-    public bool enemzSpawned;
-    public GameObject prefab;
-    public int numberOfEnemies;
+    public GameObject spawnObject;
+    [SerializeField] private float spawnPeriod;
+    [SerializeField] private int numberOfSpawns;
+    [SerializeField] private float startSpawnTime;
+    private Animator animator;
+    private Transform spawnPoint;
+    
+    
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spawnPoint = transform.Find("Spawn_Point").transform;
+        if (spawnPeriod == 0f)
+            spawnPeriod = 10f;
+        if (numberOfSpawns == 0)
+            numberOfSpawns = 1;
+        if (startSpawnTime == 0f)
+            startSpawnTime = 20f;
+    }
 
-    void Update()
-    { 
-        if (!enemzSpawned)
+    private void Update()
+    {
+        if (startSpawnTime + spawnPeriod <= Time.time)
         {
-            for (int i = 0; i < numberOfEnemies; i++)
-                Instantiate(prefab, Vector3.zero, Quaternion.identity);
-
-            enemzSpawned = true;
-            StartCoroutine(SpawnEnemy());
+            animator.SetTrigger("open");
+            startSpawnTime = Time.time;
         }
     }
 
-    private IEnumerator SpawnEnemy()
+    private void StartSpawning()
     {
-        yield return new WaitForSeconds(2f);
+        for (int i = 0; i < numberOfSpawns; i++)
+        {
+            float rand = Random.Range(-0.1f, 0.1f);
+            Instantiate(spawnObject, spawnPoint.position + new Vector3(rand, 0f, 0f), Quaternion.identity);
+        }
+            
+        StartCoroutine(CloseEntrance());   
+    }
 
-        enemzSpawned = false;
+    private IEnumerator CloseEntrance()
+    {
+        yield return new WaitForSeconds(1f);
+
+        animator.SetTrigger("close");
     }
 }
