c17b793a0931e76666054b3bf4a59e4aef8c6733
Ondřej Pavelka <flamekick97@gmail.com>
2021-03-13 15:54:40 +0100

    Created Font Loader

---
+++ Arena-Fighters-Unity-Project/Assets/User-Interface/Scripts/Font_Loader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Font_Loader : MonoBehaviour
+{
+    public Font font;
+
+    private void Awake()
+    {
+        Text[] texts = GetComponentsInChildren<Text>();
+        foreach (Text text in texts)
+            text.font = font;
+    }
+}
