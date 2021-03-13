db59524ae45cb0af57d4be9e7b839e6b68b0027a
Ondřej Pavelka <flamekick97@gmail.com>
2021-03-13 21:15:49 +0100

    Enemy Counter + Max Enemies Cap

--- Arena-Fighters-Unity-Project/Assets/Environment/Enemy_Spawner.cs
+++ Arena-Fighters-Unity-Project/Assets/Environment/Enemy_Spawner.cs
@@ -9,11 +9,12 @@ public class Enemy_Spawner : MonoBehaviour
     [SerializeField] private float startSpawnTime;
     private Animator animator;
     private Transform spawnPoint;
-    
+    private Enemy_Counter enemyCounter;
     
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        enemyCounter = GameObject.FindGameObjectWithTag("EnemyCounter").GetComponent<Enemy_Counter>();
         spawnPoint = transform.Find("Spawn_Point").transform;
         if (spawnPeriod == 0f)
             spawnPeriod = 10f;
@@ -34,12 +35,14 @@ public class Enemy_Spawner : MonoBehaviour
 
     private void StartSpawning()
     {
-        for (int i = 0; i < numberOfSpawns; i++)
+        if (enemyCounter.enemies.Length <= 40)
         {
-            float rand = Random.Range(-0.1f, 0.1f);
-            Instantiate(spawnObject, spawnPoint.position + new Vector3(rand, 0f, 0f), Quaternion.identity);
-        }
-            
+            for (int i = 0; i < numberOfSpawns; i++)
+            {
+                float rand = Random.Range(-1f, 1f);
+                Instantiate(spawnObject, spawnPoint.position + new Vector3(rand, 0f, 0f), Quaternion.identity);
+            }
+        }  
         StartCoroutine(CloseEntrance());   
     }
 
---
+++ Arena-Fighters-Unity-Project/Assets/User-Interface/Scripts/Enemy_Counter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy_Counter : MonoBehaviour
+{
+    public GameObject[] enemies;
+    private Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        text.text = enemies.Length.ToString();
+    }
+}
