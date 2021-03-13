1291d46257f9de52a55e9f7c59bc60c91db0a1c5
Ondřej Pavelka <flamekick97@gmail.com>
2021-03-13 21:33:33 +0100

    Time Counter

---
+++ Arena-Fighters-Unity-Project/Assets/User-Interface/Scripts/Time_Display.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Time_Display : MonoBehaviour
+{
+    private Text text;
+    
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        float minutes = Mathf.FloorToInt(Time.time / 60);  
+        float seconds = Mathf.FloorToInt(Time.time % 60);
+
+        text.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
