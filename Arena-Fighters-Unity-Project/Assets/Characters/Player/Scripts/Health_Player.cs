e96e3d84d0418b04c4619d9c5c4d83f51d204c7d
Ondřej Pavelka <flamekick97@gmail.com>
2021-03-13 22:46:51 +0100

    Player Death Screen

---
+++ Arena-Fighters-Unity-Project/Assets/Characters/Player/Scripts/Health_Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Health_Player : Health
+{
+    private Transform deathScreen;
+    private Score_Counter scoreCounter;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        deathScreen = GameObject.Find("Canvas").transform.Find("Death_Screen");
+        scoreCounter = GameObject.FindGameObjectWithTag("ScoreCounter").GetComponent<Score_Counter>();
+    }
+
+    private void CheckForDeath()
+    {
+        if (amount <= 0f)
+        {
+            Time.timeScale = 0f;
+            deathScreen.gameObject.SetActive(true);
+            deathScreen.Find("Final_Score").GetComponent<Text>().text = scoreCounter.score.ToString();
+            Cursor.lockState = CursorLockMode.None;    
+        }
+    }
+}
